56d04cee899f7f608ccfbef5b9b08e66271a1f2d
Hoang Van Sang <sanghv4>
2021-03-10 00:16:57 +0700

    Repair Welcome to VPBank

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Welcome to Sang");
+		get("/hello", (req, res) -> "Welcome to Vpbank");
 	}
 }
